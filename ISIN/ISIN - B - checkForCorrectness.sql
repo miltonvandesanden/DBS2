f027def1035d017d53f35219cb59fcd4729da5f2
salami350 <salami350@live.nl>
2015-06-24 23:22:01 +0200

    stufssel++

--- ISIN/ISIN - B - checkForCorrectness.sql
+++ ISIN/ISIN - B - checkForCorrectness.sql
@@ -5,10 +5,11 @@ CREATE OR REPLACE FUNCTION DBS2_checkForCorrectness (
 RETURN INTEGER
 AS
   isCorrect INTEGER := 0;
+  studentCode VARCHAR2(12);
 BEGIN
-
-  dbms_output.put_line('implementeer deze functie verder...');
-
+  IF ASCII(SUBSTR(ISIN, 1, 1)) >= 65 AND ASCII(SUBSTR(ISIN, 1, 1)) <= 90 THEN
+    isCorrect := 0;
+  END IF;
   RETURN isCorrect;
 END DBS2_checkForCorrectness;
 /
